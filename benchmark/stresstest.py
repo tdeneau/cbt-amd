753b647a5797556c38420e58a3c28edd677b202f
Tom Deneau <tom.deneau@gmail.com>
2016-03-15 09:54:22 -0700

    refactor stresstest.py

--- benchmark/stresstest.py
+++ benchmark/stresstest.py
@@ -3,6 +3,7 @@ import common
 import settings
 import monitoring
 import os
+import sys
 import logging
 
 
@@ -10,6 +11,26 @@ from cluster.ceph import Ceph
 from benchmark import Benchmark
 
 logger = logging.getLogger('cbt')
+
+class s3loop:
+    def __init__(self, testcfg):
+        logger.info ('s3loop cfg = %s' % testcfg)
+        self.gw_host = testcfg.get('gw_host', 'localhost')
+        self.gw_port = testcfg.get('gw_port', 7480)
+
+    def run(self, id, run_dir):
+        outfile = '%s/stress-s3loop-%d.out ' % (run_dir, id)
+        p = common.pdsh(settings.getnodes('clients'), 'cd /home/tom/ceph-bringup; bash s3-loop.sh %s:%s /tmp/test %s > %s 2>&1'
+                        % (self.gw_host, self.gw_port, id, outfile))
+        # logger.info('cd /home/tom/ceph-bringup; bash s3-loop.sh %s:%s /tmp/test %s > %s' % (self.gw_host, self.gw_port, id, outfile))
+        return p
+
+class s4loop:
+    def __init__(self, testcfg):
+        logger.info ('s4loop cfg = %s' % testcfg)
+
+    def run(self, id, run_dir):
+        pass
 
 class StressTest(Benchmark):
 
@@ -29,15 +50,25 @@ class StressTest(Benchmark):
         common.make_remote_dir(self.run_dir)
         logger.info('config is %s' % (self.config))
         ps = []
-        tests = ['s3-loop']
-        for t in tests:
-            tcount = self.config.get(t, 0)
-            logger.info ('running %s, %d copies' % (t, tcount))
+        tests = self.config.get('tests')
+        logger.info('tests is %s' % (tests))
+        for tname in tests.keys():
+            testcfg = tests.get(tname)
+            tcount = testcfg.get('copies', 0)
+            # instantiate test obj
+            # first get class based on test name
+            module = sys.modules[globals()['__name__']]
+            cls = getattr(module, tname, None)
+            if (cls is None):
+                logger.fatal ('FATAL: no stresstest named %s' % (tname))
+                sys.exit()
+
+            testobj = cls(testcfg)
+            logger.info ('%s running %s, %d copies' % (testobj, tname, tcount))
             for i in xrange(tcount):
-                outfile = '%s/stress-%s-%d.out ' % (self.run_dir, t, i)
-                logger.info ('%s, copy #%d writing to %s' % (t, i, outfile))
-                if (t == 's3-loop'):
-                    p = common.pdsh(settings.getnodes('clients'), 'cd /home/tom/ceph-bringup; bash s3-loop.sh Intel-2P-Sandy-Bridge-04:7480 /tmp/test %s > %s' % (i, outfile))
+                logger.info ('%s, copy #%d' % (tname, i))
+                p = testobj.run(i, self.run_dir)
+                if p:
                     ps.append(p)
         for p in ps:
             p.wait()
